2d2b7df8b771158332fc95ae180f75e0284189ef
eomielan <eomielan@gmail.com>
2024-10-27 14:33:04 -0700

    Update app.test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App.js";
 
-test("renders learn react link", () => {
+test("renders Sender Page heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Sender Page/i);
+  expect(headingElement).toBeInTheDocument();
 });
