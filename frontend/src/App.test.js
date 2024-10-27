b49094119631e6058e22c8cfeb9f91b6cf8dd773
eomielan <eomielan@gmail.com>
2024-10-27 14:41:17 -0700

    Update tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,31 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App.js";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import SenderPage from "./components/SenderPage";
+import ReceiverPage from "./components/ReceiverPage";
 
-test("renders Sender Page heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Sender Page/i);
-  expect(headingElement).toBeInTheDocument();
+describe("App Navigation", () => {
+  test("renders SenderPage when navigating to '/'", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<SenderPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const senderHeading = screen.getByText(/Sender Page/i);
+    expect(senderHeading).toBeInTheDocument();
+  });
+
+  test("renders ReceiverPage when navigating to '/receiver'", () => {
+    render(
+      <MemoryRouter initialEntries={["/receiver"]}>
+        <Routes>
+          <Route path="/receiver" element={<ReceiverPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const receiverHeading = screen.getByText(/Receiver Page/i);
+    expect(receiverHeading).toBeInTheDocument();
+  });
 });
