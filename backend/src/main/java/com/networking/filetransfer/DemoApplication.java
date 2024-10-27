6fe9564ba9608f5cebca33e3ae5919c050fbd213
eomielan <eomielan@gmail.com>
2024-10-26 22:25:11 -0700

    Add tcp-over-udp to backend

--- backend/src/main/java/com/networking/filetransfer/DemoApplication.java
+++ backend/src/main/java/com/networking/filetransfer/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.networking.filetransfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/networking/filetransfer/DemoApplicationTests.java
+++ backend/src/test/java/com/networking/filetransfer/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.networking.filetransfer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
