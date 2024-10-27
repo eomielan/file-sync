2c4d9afa1e6938af84bbba53576a86225e4e49c0
eomielan <eomielan@gmail.com>
2024-10-27 13:33:02 -0700

    Remove extra space

--- backend/src/test/java/com/networking/filetransfer/utils/TransferExecutorTest.java
+++ backend/src/test/java/com/networking/filetransfer/utils/TransferExecutorTest.java
@@ -10,14 +10,12 @@ public class TransferExecutorTest {
     @Test
     void testSendFile_Success() throws InterruptedException {
         String result = TransferExecutor.sendFile("/path/to/testfile.txt", "localhost", 12345, 1024);
-
         assertTrue(result.contains("File sent successfully"));
     }
 
     @Test
     void testReceiveFile_Success() throws InterruptedException {
         String result = TransferExecutor.receiveFile("/path/to/downloaded_file.txt", 12345);
-
         assertTrue(result.contains("File received successfully"));
     }
 }
