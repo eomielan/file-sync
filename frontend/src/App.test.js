64273e33f737693d0364b69ab54e5aa55c9005f7
eomielan <eomielan@gmail.com>
2024-10-28 20:10:31 -0700

    Update App.js and add new tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import SenderPage from "./components/SenderPage";
 import ReceiverPage from "./components/ReceiverPage";
+import UploadPage from "./components/UploadPage";
+import DownloadPage from "./components/DownloadPage";
 
 describe("App Navigation", () => {
   test("renders SenderPage when navigating to '/'", () => {
@@ -27,5 +29,29 @@ describe("App Navigation", () => {
     );
     const receiverHeading = screen.getByText(/Receiver Page/i);
     expect(receiverHeading).toBeInTheDocument();
+  });
+
+  test("renders UploadPage when navigating to '/upload'", () => {
+    render(
+      <MemoryRouter initialEntries={["/upload"]}>
+        <Routes>
+          <Route path="/upload" element={<UploadPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const uploadHeading = screen.getByText(/Upload to S3/i);
+    expect(uploadHeading).toBeInTheDocument();
+  });
+
+  test("renders DownloadPage when navigating to '/download'", () => {
+    render(
+      <MemoryRouter initialEntries={["/download"]}>
+        <Routes>
+          <Route path="/download" element={<DownloadPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    const downloadHeading = screen.getByText(/Download from S3/i);
+    expect(downloadHeading).toBeInTheDocument();
   });
 });
